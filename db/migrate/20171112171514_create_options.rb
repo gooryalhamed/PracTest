f62a2e896a8a59fdca6168ccb8ce04af27c0f0b0
gooryalhamed <gooryalhamed@hotmail.com>
2017-11-13 16:06:49 +0300

    update migrate file and migrating database

--- db/migrate/20171112171514_create_options.rb
+++ db/migrate/20171112171514_create_options.rb
@@ -2,7 +2,7 @@ class CreateOptions < ActiveRecord::Migration[5.1]
   def change
     create_table :options do |t|
       t.string :body
-      t.correct? :boolean
+      t.boolean :correct?
       t.integer :question_id
 
       t.timestamps
